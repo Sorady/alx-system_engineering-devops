a29b5d421d647ef2d5b040106d8ab17ef2ed463d
Sorady <125706196+Sorady@users.noreply.github.com>
2023-12-17 00:07:16 +0200

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,52 +1,54 @@
 #!/usr/bin/python3
-"""
-    Recursive function that queries the Reddit API, parses the title of all hot
-    articles, and prints a sorted count of given keywords (case-insensitive,
-    delimited by spaces. Javascript should count as javascript, but java should
-    not).
-"""
-import re
+""" raddit api"""
+
+import json
 import requests
-headers = {'user-agent': 'ubuntu:hbtn:v1.0\
- (by /u/Brandixitor)'}
 
 
-def count_words(subreddit, word_list, after='', occurs={}):
-    """
-        Recursive function that queries the Reddit API, parses the title of all
-        hot articles, and prints a sorted count of given keywords
-        (case-insensitive, delimited by spaces.
-        Javascript should count as javascript, but java should not).
-    """
-    url = 'https://api.reddit.com/r/' + subreddit + '?limit=100&after=' + after
-    response = requests.get(url, headers=headers)
-    try:
-        data = response.json()
-    except:
-        return
-    if (str(response.status_code) == '404'):
-        return
-    dataLength = len(data['data']['children'])
-    if (dataLength is 0):
-        return
-    for i in range(0, dataLength):
-        try:
-            get_title = data['data']['children'][i]['data']['title']
-            for a in word_list:
-                try:
-                    occurs[a]
-                except KeyError:
-                    occurs[a] = 0
-                finally:
-                    occurs[a] += re.subn(r'(?i)(?<!\S)\b{}\b(?!\S)'.format(a),
-                                         '', get_title)[1]
-        except:
-            pass
-    afterVal = data['data']['after']
-    if (afterVal is not None):
-        return count_words(subreddit, word_list, afterVal, occurs)
-    else:
-        for key in sorted(occurs, key=lambda k: (-occurs[k], k)):
-            if (occurs[key] > 0):
-                print("{}: {}".format(key, occurs[key]))
-        return
+def count_words(subreddit, word_list, after="", count=[]):
+    """count all words"""
+
+    if after == "":
+        count = [0] * len(word_list)
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    request = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'user-agent': 'bhalut'})
+
+    if request.status_code == 200:
+        data = request.json()
+
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == word.lower():
+                        count[i] += 1
+
+        after = data['data']['after']
+        if after is None:
+            save = []
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
+
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if (count[j] > count[i] or
+                            (word_list[i] > word_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = aux
+
+            for i in range(len(word_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
+        else:
+            count_words(subreddit, word_list, after, count)
