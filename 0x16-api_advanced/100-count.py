43535de979c80da524bafee11e9ab6e3a39fd62c
Sorady <125706196+Sorady@users.noreply.github.com>
2023-12-17 00:18:25 +0200

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,89 +1,72 @@
 #!/usr/bin/python3
 """
-This function queries the Reddit API and  parses the
-title of all hot articles, and prints a sorted count
-of given keywords (case-insensitive, delimited by spaces.
-Javascript should count as javascript, but java should not).
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
 """
+import re
 import requests
+import sys
 
 
-def count_words(subreddit, word_list, hot_list=[], after=""):
-    """
-    Returns the sorted count of keywords appearing in
-    titles of hot posts in Reddit
+def add_title(dictionary, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
 
-    Args:
-        subreddit (string): The name of the subreddit
-        wordlist (list): The list of keywords to search for
-        hot_list (list): List of all posts of a subredit
-        after (string): The id of the parameter in url used
-                        to query for next page items
-    Return:
-           word_count (dict): sorted count of given keywords
-    """
-    if subreddit is None:
+    title = hot_posts[0]['data']['title'].split()
+    for word in title:
+        for key in dictionary.keys():
+            c = re.compile("^{}$".format(key), re.I)
+            if c.findall(word):
+                dictionary[key] += 1
+    hot_posts.pop(0)
+    add_title(dictionary, hot_posts)
+
+
+def recurse(subreddit, dictionary, after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
         return None
 
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    user_agent = {'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.0)\
-     AppleWebKit/538.0.0 (KHTML, like Gecko) \
-     Chrome/17.0.821.0 Safari/538.0.0'}
-
-    try:
-        response = requests.get(url, headers=user_agent,
-                                allow_redirects=False,
-                                params={'limit': 100,
-                                        'after': after})
-
-        if response.status_code == 200:
-            results = response.json()
-            top_posts = results.get('data').get('children')
-
-            for post in top_posts:
-                hot_list.append(post.get('data').get('title'))
-
-            after = results.get('data').get('after')
-
-            if after is None:
-                keywords_dict = {}
-                for word in word_list:
-                    word = word.lower()
-                    for post_title in hot_list:
-                        count = count_occurence(word, post_title)
-                        if count > 0:
-                            if keywords_dict.get(word):
-                                keywords_dict[word] += count
-                            else:
-                                keywords_dict[word] = count
-
-                sorted_dict = sorted(keywords_dict.items(),
-                                     key=lambda item: (-item[1], item[0]))
-
-                for key, value in sorted_dict:
-                    print("{}: {}".format(key, value))
-            else:
-                return count_words(subreddit, word_list, hot_list, after)
-        else:
-            return None
-    except Exception:
-        return None
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(dictionary, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return
+    recurse(subreddit, dictionary, after=after)
 
 
-def count_occurence(key, title):
-    """
-    Returns the number of occurence of a word in a sentence
+def count_words(subreddit, word_list):
+    """ Init function """
+    dictionary = {}
 
-    Args:
-        key (string): The word to count number of occurence
-        title (string): The sentence to count the occurences
+    for word in word_list:
+        dictionary[word] = 0
 
-    Return:
-           count (int): The number of times the word occurs
-    """
-    words = title.split()
-    count = 0
-    for word in words:
-        if key.lower() == word.lower():
-            count += 1
-    return count
+    recurse(subreddit, dictionary)
+
+    l = sorted(dictionary.items(), key=lambda kv: kv[1])
+    l.reverse()
+
+    if len(l) != 0:
+        for item in l:
+            if item[1] is not 0:
+                print("{}: {}".format(item[0], item[1]))
+    else:
+        print("")
