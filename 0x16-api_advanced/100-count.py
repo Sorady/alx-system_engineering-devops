38246e6ee4995d3c0fbb32baedee2566660f2441
Sorady <125706196+Sorady@users.noreply.github.com>
2023-12-16 23:55:51 +0200

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,118 +1,48 @@
 #!/usr/bin/python3
-""" Recursive function that queries the Reddit API, parses the title of all
-hot articles, and prints a sorted count of given keywords (case-insensitive,
-delimited by spaces. Javascript should count as javascript,
-but java should not)
+"""
+100-count
 """
 import requests
 
-
-def f_after(subreddit, hot_list, after=""):
-    """Recursive function find to length of hot_list
-    Args:
-        subreddit: Account to search
-        hot_list: hot list of Reddit
-        after: next page of API
+def count_words(subreddit, word_list, after=None, count={}):
     """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
-    if after is not None:
-        data = requests.get(url,
-                            headers={'User-Agent': 'AgentMEGO'},
-                            params={'after': after})
-        if data.status_code != 200:
-            return None
-        after = data.json().get('data').get('after')
-        JSON = data.json().get('data').get('children')
-        for value in JSON:
-            hot_list.append(value.get('data').get('title'))
-        return f_after(subreddit, hot_list, after)
-    return (hot_list)
-
-
-def sort_dict_by_value(d, reverse=False):
-    return dict(sorted(
-        d.items(), key=lambda x: x[1], reverse=reverse))
-
-
-def count_words(subreddit, word_list):
-    """Returns a list containing the titles of all hot articles for
-    a given subreddit. If no results are found for the given subreddit,
-    the function should return None.
-
-    Args:
-        subreddit: Account to search
+    Recursive function that queries the Reddit API, parses the title of all hot
+    articles, and prints a sorted count of given keywords
     """
-    if subreddit is None or type(subreddit) is not str:
+    # Base case: invalid subreddit or no more pages
+    if after is None and count == {}:
         return None
-
-    data = f_after(subreddit, [], "")
-    data = " ".join(data)
-    data = data.split(" ")
-
-    data_list = {}
-    num = 0
-
-    for i in word_list:
-        """
-        search = [char for char in i]
-        re_search = ""
-        for word in search:
-            if word.isalpha():
-                re_search += "[{}{}]".format(word.lower(), word.upper())
-            else:
-                re_search += word
-        for val in data:
-            text = re.compile("\\W" + re_search + "\\W")
-            if text.findall(val) != []:
-        num += 1
-        """
-        for val in data:
-            i = i.lower()
-            if val.lower() == i:
-                num += 1
-        if i in data_list.keys():
-            data_list[i] = data_list[i] + num
-        else:
-            data_list[i] = num
-        num = 0
-
-    new_data = data_list
-    tmp_key = ""
-    tmp_value = 0
-    char1 = []
-    char2 = []
-    x = 0
-    newnew = {}
-    new_data = sort_dict_by_value(new_data, True)
-    for key, value in new_data.items():
-        if value == 0:
-            continue
-        newnew[key] = value
-        if value == tmp_value:
-            char1 = [char for char in key]
-            char2 = [charr for charr in tmp_key]
-            c1 = ord(char1[x])
-            c2 = ord(char2[x])
-            c3 = 0
-            while (c1 and c2):
-                c1 = ord(char1[x])
-                c2 = ord(char2[x])
-                if c1 < c2:
-                    c3 = 0
-                    break
-                elif c1 > c2:
-                    c3 = 1
-                    break
-                x += 1
-            if c3 == 0:
-                del newnew[tmp_key]
-                newnew[key] = value
-                newnew[tmp_key] = tmp_value
-                tmp_key = tmp_key
-                tmp_value = tmp_value
-                continue
-        tmp_key = key
-        tmp_value = value
-
-    for a, b in newnew.items():
-        print("{}: {}".format(a, b))
+    # Set the headers and parameters for the request
+    headers = {'User-Agent': 'python3:100-count:v1.0'}
+    params = {'limit': 100, 'after': after}
+    # Make the request to the subreddit's hot articles
+    response = requests.get('https://www.reddit.com/r/{}/hot.json'.format(subreddit), headers=headers, params=params, allow_redirects=False)
+    # Check the status code
+    if response.status_code != 200:
+        return None
+    # Get the data as a dictionary
+    data = response.json().get('data')
+    # Get the list of hot articles
+    articles = data.get('children')
+    # Loop through the articles
+    for article in articles:
+        # Get the title of the article
+        title = article.get('data').get('title').lower()
+        # Loop through the word list
+        for word in word_list:
+            # Check if the word is in the title
+            if word.lower() in title.split():
+                # Increment the count of the word
+                count[word.lower()] = count.get(word.lower(), 0) + title.split().count(word.lower())
+    # Get the next page
+    after = data.get('after')
+    # Recursive call with the next page and the current count
+    count_words(subreddit, word_list, after, count)
+    # Print the results
+    if after is None:
+        # Sort the count by value and then by key
+        sorted_count = sorted(count.items(), key=lambda x: (-x[1], x[0]))
+        # Loop through the sorted count
+        for key, value in sorted_count:
+            # Print the word and the count
+            print('{}: {}'.format(key, value))
