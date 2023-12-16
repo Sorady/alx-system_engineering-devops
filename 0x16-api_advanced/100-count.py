61f76efde880740ee434029cc97b7ea031571d75
Sorady <125706196+Sorady@users.noreply.github.com>
2023-12-17 00:04:57 +0200

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,39 +1,52 @@
 #!/usr/bin/python3
-""" recursive function that queries the Reddit API """
+"""
+    Recursive function that queries the Reddit API, parses the title of all hot
+    articles, and prints a sorted count of given keywords (case-insensitive,
+    delimited by spaces. Javascript should count as javascript, but java should
+    not).
+"""
+import re
 import requests
+headers = {'user-agent': 'ubuntu:hbtn:v1.0\
+ (by /u/Brandixitor)'}
 
 
-def count_words(subreddit, word_list, passed_after="", word_dict={}):
-    """ returns title of 10 hot posts """
-    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
-    headers = {"user-agent": 'me'}
-    after = passed_after
-    params = {"after": after}
-    data = requests.get(url, headers=headers, params=params,
-                        allow_redirects=False)
-    if data.status_code != 200:
-        return
-    data = data.json()
+def count_words(subreddit, word_list, after='', occurs={}):
+    """
+        Recursive function that queries the Reddit API, parses the title of all
+        hot articles, and prints a sorted count of given keywords
+        (case-insensitive, delimited by spaces.
+        Javascript should count as javascript, but java should not).
+    """
+    url = 'https://api.reddit.com/r/' + subreddit + '?limit=100&after=' + after
+    response = requests.get(url, headers=headers)
     try:
-        for item in data['data']['children']:
-            full_title = item['data']['title']
-            title_list = full_title.split()
-            for title in title_list:
-                for word in word_list:
-                    if title.lower() == word.lower():
-                        if word.lower() in word_dict:
-                            word = word.lower()
-                            word_dict[word] = word_dict[word] + 1
-                        else:
-                            word_dict[word.lower()] = 1
-        passed_after = data['data']['after']
-        if passed_after is None:
-            sorted_list = sorted(word_dict.items(), key=lambda a: a[1],
-                                 reverse=True)
-            for item in sorted_list:
-                print("{}: {}".format(item[0], item[1]))
-            return
-        else:
-            count_words(subreddit, word_list, passed_after, word_dict)
-    except Exception:
-        return None
+        data = response.json()
+    except:
+        return
+    if (str(response.status_code) == '404'):
+        return
+    dataLength = len(data['data']['children'])
+    if (dataLength is 0):
+        return
+    for i in range(0, dataLength):
+        try:
+            get_title = data['data']['children'][i]['data']['title']
+            for a in word_list:
+                try:
+                    occurs[a]
+                except KeyError:
+                    occurs[a] = 0
+                finally:
+                    occurs[a] += re.subn(r'(?i)(?<!\S)\b{}\b(?!\S)'.format(a),
+                                         '', get_title)[1]
+        except:
+            pass
+    afterVal = data['data']['after']
+    if (afterVal is not None):
+        return count_words(subreddit, word_list, afterVal, occurs)
+    else:
+        for key in sorted(occurs, key=lambda k: (-occurs[k], k)):
+            if (occurs[key] > 0):
+                print("{}: {}".format(key, occurs[key]))
+        return
