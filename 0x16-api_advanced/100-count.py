11ead8c53b2038389ffcf401205002fb7448d882
Sorady <125706196+Sorady@users.noreply.github.com>
2023-12-17 00:04:11 +0200

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
-"""
-Python script that, using this REST API, for a given subreddit,
-returns all its hot posts for a given subreddit.
-"""
+""" recursive function that queries the Reddit API """
 import requests
 
 
-def count_words(subreddit, word_list, after=None, count_dict=None):
-    if count_dict is None:
-        count_dict = {}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after} if after else {}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        after = data['data']['after']
-        for post in posts:
-            title = post['data']['title'].lower()
-            for word in word_list:
-                if word.lower() in title:
-                    index = title.index(word.lower())
-                    if (index == 0 or not title[
-                        index-1].isalpha()) and (index+len(word) == len(
-                            title) or not title[index+len(word)].isalpha()):
-                        if word.lower() in count_dict:
-                            count_dict[word.lower()] += 1
+def count_words(subreddit, word_list, passed_after="", word_dict={}):
+    """ returns title of 10 hot posts """
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    headers = {"user-agent": 'me'}
+    after = passed_after
+    params = {"after": after}
+    data = requests.get(url, headers=headers, params=params,
+                        allow_redirects=False)
+    if data.status_code != 200:
+        return
+    data = data.json()
+    try:
+        for item in data['data']['children']:
+            full_title = item['data']['title']
+            title_list = full_title.split()
+            for title in title_list:
+                for word in word_list:
+                    if title.lower() == word.lower():
+                        if word.lower() in word_dict:
+                            word = word.lower()
+                            word_dict[word] = word_dict[word] + 1
                         else:
-                            count_dict[word.lower()] = 1
-        if after:
-            return count_words(subreddit, word_list, after, count_dict)
+                            word_dict[word.lower()] = 1
+        passed_after = data['data']['after']
+        if passed_after is None:
+            sorted_list = sorted(word_dict.items(), key=lambda a: a[1],
+                                 reverse=True)
+            for item in sorted_list:
+                print("{}: {}".format(item[0], item[1]))
+            return
         else:
-            sorted_counts = sorted(
-                count_dict.items(), key=lambda x: (-x[1], x[0]))
-            for count in sorted_counts:
-                print(f"{count[0]}: {count[1]}")
-    else:
-        print(None)
+            count_words(subreddit, word_list, passed_after, word_dict)
+    except Exception:
+        return None
